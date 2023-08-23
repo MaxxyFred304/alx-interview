65b7ad27f2f4936c069dec5f9ddc99cc0160f36a
root <maxfred304@gmail.com>
2023-08-23 07:03:34 +0000

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,18 +2,22 @@
 
 
 def makeChange(coins, total):
-    if total <= 0:
+    """
+    Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
+    :param coins: list of integers representing the values of the coins in your possession
+    :param total: integer representing the amount you want to make change for
+    :return: fewest number of coins needed to meet total
+             If total is 0 or less, return 0
+             If total cannot be met by any number of coins you have, return -1
+    """
+    if total < 0:
         return 0
-
-    
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
+    if total == 0:
+        return 0
+    min_coins = float('inf')
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    if dp[total] == float('inf'):
-        return -1
-    else:
-        return dp[total]
+        if total - coin >= 0:
+            num_coins = makeChange(coins, total - coin)
+            if num_coins < min_coins:
+                min_coins = num_coins
+    return min_coins + 1 if min_coins != float('inf') else -1
